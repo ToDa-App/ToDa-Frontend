3b55c4d63ddaffd2e3da672bf080b4791fd09b9b
habiba <habibaahmedattia5@gmail.com>
2025-06-16 02:55:58 +0300

    Edit and delete task from task details

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -2,12 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TaskService } from '../core/services/task.service';
 import { Router } from '@angular/router';
+import { TaskSummary } from '../core/services/task.service';
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent implements OnInit {
+  tasks: TaskSummary[] = [];
+  statusFilter: string = '';
+  priorityFilter: string = '';
+  currentPage = 0;
   task: any;
   constructor(
     private route: ActivatedRoute,
@@ -32,14 +37,29 @@ export class TaskDetailsComponent implements OnInit {
       console.error('No task ID provided');
     }
   }
-  editTask(): void {
-    sessionStorage.setItem('editTaskId', this.task.id.toString());
-    this.router.navigate(['/edit-task']);
+  editTask(task: any): void {
+    sessionStorage.setItem('taskId', task.id.toString());
+    this.router.navigate(['/task-edit']);
   }
-  deleteTask(): void {
+  loadTasks(): void {
+    this.taskService.getActiveTasks(this.currentPage, this.statusFilter, this.priorityFilter)
+      .subscribe({
+        next: (res: any) => {
+          this.tasks = res.data?.content ?? [];
+        },
+        error: err => {
+          console.error('Failed to fetch tasks:', err);
+        }
+      });
+
+  }
+  deleteTask(id: number): void {
     if (confirm('Are you sure you want to delete this task?')) {
-      this.taskService.deleteTask(this.task.id).subscribe({
-        next: () => this.router.navigate(['/dashboard']),
+      this.taskService.deleteTask(id).subscribe({
+        next: () => {
+          this.loadTasks();
+          this.router.navigate(['/dashboard']);
+        },
         error: (err) => console.error('Failed to delete task', err)
       });
     }
